a47dd8d19bf0ba18db7e6f24bdfd2f36c16a5881
rMaloberti <r.maloberti@yahoo.com>
2022-12-12 06:03:57 +0100

    feat(router): add the home route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Router>
   </React.StrictMode>
 );
 
