b0d0af54a0cd91df47110af0723fb44402eb2af9
rMaloberti <r.maloberti@yahoo.com>
2023-01-04 16:28:30 +0100

    feat(error): create the error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import './Error.css';
 
@@ -5,6 +6,15 @@ function Error() {
   return (
     <div className="page">
       <Header />
+      <div className="error-content">
+        <div className="top-section">
+          <h1 className="top-section__title">404</h1>
+          <p className="top-section__text">Oups! La page que vous demandez n'existe pas.</p>
+        </div>
+        <div className="bottom-section">
+          <Link className="bottom-section__link" to="/">Retourner sur la page d’accueil</Link>
+        </div>
+      </div>
     </div>
   );
 }
