5f15c4a042679e7b70279447999022e0267172fb
rMaloberti <r.maloberti@yahoo.com>
2023-01-05 16:50:22 +0100

    feat(logement): add the tag component

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from 'react-router-dom';
 import Carousel from '../components/Carousel';
 import Header from '../components/Header';
 import Host from '../components/Host';
+import Tag from '../components/Tag';
 import Title from '../components/Title';
 import { getLogement } from '../data/data';
 import './Logement.css';
@@ -29,6 +30,11 @@ function Logement() {
         <div className="infos">
           <div className="infos__logement">
             <Title title={logement.title} location={logement.location} />
+            <div className="infos__logement__tags">
+              {logement.tags.map((tag) => (
+                <Tag tag={tag} />
+              ))}
+            </div>
           </div>
           <div className="infos__hosting">
             <Host host={logement.host} />
