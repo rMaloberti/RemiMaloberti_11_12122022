ddad309d2c48139dedd04baf083d6af7021d6c7b
rMaloberti <r.maloberti@yahoo.com>
2023-01-14 16:47:53 +0100

    feat(logement): optimize dropdowns component structure

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from 'react-router-dom';
 import Carousel from '../components/Carousel';
-import DetailsDropdown from '../components/DetailsDropdown';
+import Dropdown from '../components/Dropdown';
 import Header from '../components/Header';
 import Host from '../components/Host';
 import Rating from '../components/Rating';
@@ -44,8 +44,20 @@ function Logement() {
           </div>
         </div>
         <div className="details">
-          <DetailsDropdown title="Description" content={logement.description} isList={false} />
-          <DetailsDropdown title="Équipements" content={logement.equipments} isList={true} />
+          <Dropdown
+            title="Description"
+            content={logement.description}
+            isList={false}
+            isDetailsDropdown={true}
+            defaultStatus="opened"
+          />
+          <Dropdown
+            title="Équipements"
+            content={logement.equipments}
+            isList={true}
+            isDetailsDropdown={true}
+            defaultStatus="opened"
+          />
         </div>
       </div>
     </div>
