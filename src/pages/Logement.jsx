446cc80477e03304d4eb6f5cb36a080af4384b94
rMaloberti <r.maloberti@yahoo.com>
2023-01-14 15:32:26 +0100

    feat(logement): create the rating component

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from 'react-router-dom';
 import Carousel from '../components/Carousel';
 import Header from '../components/Header';
 import Host from '../components/Host';
+import Rating from '../components/Rating';
 import Tag from '../components/Tag';
 import Title from '../components/Title';
 import { getLogement } from '../data/data';
@@ -38,6 +39,7 @@ function Logement() {
           </div>
           <div className="infos__hosting">
             <Host host={logement.host} />
+            <Rating rating={logement.rating} />
           </div>
         </div>
         <div className="details"></div>
